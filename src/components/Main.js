df577f61619e449be276bba9be143ebed4109521
SouvikGolui8899 <souvikgolui88@gmail.com>
2018-03-14 21:47:32 +0530

    src/components/Main.js commited

--- src/components/Main.js
+++ src/components/Main.js
@@ -29,6 +29,27 @@ export default class Main extends React.Component{
   componentDidMount(){
     this.renderProperties();
   }
+  
+  handleClick(property) {
+	const myWindow = window.open("","_blank", "toolbar=yes,scrollbars=yes,resizable=yes");
+	const propertyType = property.type !== "" ? "<p style='font-size: 1.5em;text-align: center;'><b>Type:</b> "+property.type+"</p>":"";
+	myWindow.document.write(
+	"<p style='font-size: 1.875em'> "+property.headline+"</p>"+
+	'<img style="display:block;margin-left:auto;margin-right:auto;width:60%;" src='+property.picture_large+' alt="property"/>'+
+	"<p style='font-size:1.5em;text-align: center;'><b>Title:</b> "+property.title+"</p>"+
+	"<p style='font-size: 1.5em;text-align: center;'><b>Address:</b> "+ property.address + "</p>"+
+	propertyType+
+	"<p style='font-size: 1.5em'><b>Property Highlights</b></p>"+
+	"<p style='font-size: 1.5em'>"+
+	"<ul style='font-size: 1.5em'>"+
+	  "<li style='font-size: 1.5em>"+"Bedrooms: "+property.floorplan.bedrooms+"</li>"+
+	  "<li style='font-size: 1.5em>"+"Bathrooms: "+property.floorplan.bathrooms+"</li>"+
+	"</ul>"+
+	"</p>"+
+	"<p style='font-size: 1.5em'><b>Property:</b> #"+property.id+"</p>"+
+	'<a href="'+property.application_url+'">Apply</a>'
+	);
+  }
 
   render(){
     return(
@@ -38,7 +59,10 @@ export default class Main extends React.Component{
             this.state.properties.map(property=>{
               return (
                 <Grid key={property.id} item xs={12} sm={6}>
-                  <img style={styles.property} src={property.picture} alt="property"/>
+				  <img style={styles.property} src={property.picture} alt="property" onClick={() => this.handleClick(property)}/>
+				  <p><b>Title:</b> {property.title} </p>
+				  <p><b>Address:</b> {property.address} </p>
+				 {property.type !== "" ? <p><b>Type:</b> {property.type} </p> : ""}
                 </Grid>
               )
             })
